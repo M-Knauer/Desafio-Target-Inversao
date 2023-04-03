e1a5d3ef66bb629acfdc3a2d86d76d43b3108d04
unknown <marcelocrisknauer@gmail.com>
2023-04-03 01:09:25 -0300

    First commit

---
+++ src/inversao/main/Main.java
@@ -0,0 +1,28 @@
+package inversao.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.print("Digite uma string: ");
+        String s = sc.nextLine();
+        
+        sc.close();
+        char[] arrayCaracteres = s.toCharArray();
+
+        for (int i = 0, j = arrayCaracteres.length - 1; i < j; i++, j--) {
+            // Troca os caracteres nas posições i e j
+            char temp = arrayCaracteres[i];
+            arrayCaracteres[i] = arrayCaracteres[j];
+            arrayCaracteres[j] = temp;
+        }
+
+        String stringInvertida = new String(arrayCaracteres);
+        System.out.println("String invertida: " + stringInvertida);
+        
+    }
+
+}
